41e1e1728d47e448c24f4d56edb8ffd7a907ff5e
brunorosilva <b.rosilva1@gmail.com>
2024-07-02 17:11:56 -0300

    single folder training

--- makeitsports_bot/data/data.py
+++ makeitsports_bot/data/data.py
@@ -4,11 +4,10 @@ import numpy as np
 
 
 def get_data_from_local():
-    right_data = [f"{BASE_PATH}/right/{fn}" for fn in os.listdir(f"{BASE_PATH}/right")]
-    top_data = [f"{BASE_PATH}/top/{fn}" for fn in os.listdir(f"{BASE_PATH}/top")]
-
-    x = np.array(right_data + top_data)
-    y = np.array([ex.replace("right", "left").replace("top", "bottom") for ex in x])
+    left_or_top_data = [f"{BASE_PATH}/splits/left_or_top/{fn}" for fn in os.listdir(f"{BASE_PATH}/splits/left_or_top")]
+    
+    x = np.array(left_or_top_data)
+    y = np.array([ex.replace("left_or_top", "right_or_bottom") for ex in x])
 
     data = np.array([x, y])
     return data
